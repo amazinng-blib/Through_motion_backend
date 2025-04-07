7f31b9252d46f440f4c9baf561495b9da1a9b263
Nwankwo Ernest Onyebuchi <nwankwoernest2020@gmail.com>
2025-04-07 18:42:24 +0100

    feat: fix a bug in login route

--- src/services/authService/loginService.ts
+++ src/services/authService/loginService.ts
@@ -1,6 +1,4 @@
 import { AppError } from '../../middleware/errorHandler';
-import Plans from '../../models/planModel';
-import Subscriptions from '../../models/subscriptions';
 import User from '../../models/user';
 import {
   generateAccessToken,
@@ -12,18 +10,6 @@ import bcrypt from 'bcryptjs';
 export async function loginService(input: LoginType) {
   const user = await User.findOne({
     where: { email: input?.email },
-    include: [
-      {
-        model: Subscriptions,
-        as: 'subscriptions',
-        include: [
-          {
-            model: Plans,
-            as: 'plan',
-          },
-        ],
-      },
-    ],
   });
   if (!user) {
     throw new AppError('User Not found', 404);
